b3215a94ea0d7acb43ea41374d3e2553bc1334f0
pedroroblesduten <pedroroblesduten@gmail.com>
2023-01-01 20:07:44 -0300

    ok

--- Denoising_diffusion_probabilistic_models/ddpm.py
+++ Denoising_diffusion_probabilistic_models/ddpm.py
@@ -1,6 +1,9 @@
 import torch
 import torch.nn as nn
 from tqdm import tqdm
+from unet import UNet
+from utils import get_data, setup_logging, save_images
+import torch.optim as optim
 # import logging
 
 #Following the implemenation from: https://github.com/dome272/Diffusion-Models-pytorch
@@ -24,13 +27,13 @@ class Diffusion:
         return torch.linspace(self.beta_start, self.beta_end, self.noise_steps)
 
     def noise_images(self, x, t):
-        sqrt_alpha_hat = torch.sqrt(self.alpha_hat[t])[:, None, None, None, None]
-        sqrt_one_minus_alpha_hat = torch.sqrt(1. - self.alpha_hat[t])[:, None, None, None]
+        sqrt_alpha_hat = torch.sqrt(self.alpha_hat[t])[:, None, None, None]
+        sqrt_one_minus_alpha_hat = torch.sqrt(1 - self.alpha_hat[t])[:, None, None, None]
         e = torch.randn_like(x)
         
         # Using reparametrization trick for get noised image
         noise_image = sqrt_alpha_hat*x + sqrt_one_minus_alpha_hat*e
-        return noise_image
+        return noise_image, e
     
     def sample_t(self, n):
         t = torch.randint(low=1, high=self.noise_steps, size=(n,))
@@ -43,7 +46,7 @@ class Diffusion:
             x = torch.randn((n, 3, self.img_size, self.img_size)).to(self.device) #Initializing X_T from normal distribution
             for i in tqdm(reversed(range(1, self.noise_steps)), position=0):
                 t = (torch.ones(n)*i).long().to(device)
-                predicted_noise = model(x, t)
+                predicted_noise = model(x, t, verbose=True)
                 alpha = self.alpha[t][:, None, None, None]
                 alpha_hat = self.alpha_hat[t][:, None, None, None]
                 beta = self.beta[t][:, None, None, None]
@@ -59,5 +62,45 @@ class Diffusion:
 
 
 
+def train(args):
+    device = args.device
+    dataloader = get_data(args)
+    model = UNet(verbose=False).to(device)
+    optimizer = optim.AdamW(model.parameters(), lr=args.lr)
+    mse = nn.MSELoss()
+    diffusion = Diffusion(img_size=args.image_size, device=device)
+    l = len(dataloader)
 
-       
+    for epoch in range(args.epochs):
+        pbar = tqdm(dataloader)
+        for i, (images, _) in enumerate(pbar):
+            images = images.to(device)
+            t = diffusion.sample_t(images.shape[0]).to(device)
+            x_t, noise = diffusion.noise_images(images, t)
+            predicted_noise = model(x_t, t)
+
+            loss = mse(noise, predicted_noise)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+             
+            pbar.set_postfix(MSE=loss.item())
+        sampled_images = diffusion.sample(model, n=image.shape[0])
+        save_images(sampled_images, os.path.join('results', args.run_name, f'{epoch}.jpg'))
+        torch.save(model.state_dict(), os.path.join('models', args.run_name, f'ckpt.pt'))
+
+def launch():
+    import argparse
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+    args.run_name = r"C:\Users\pedro\OneDrive\Área de Trabalho\diffusion_images"
+    args.epochs = 100
+    args.batch_size = 2
+    args.image_size = 64
+    args.dataset_path = r"C:\Users\pedro\OneDrive\Área de Trabalho\flowers"
+    args.device = "cuda"
+    args.lr = 3e-4
+    train(args)
+
+if __name__ == '__main__':
+    launch()
--- Denoising_diffusion_probabilistic_models/unet.py
+++ Denoising_diffusion_probabilistic_models/unet.py
@@ -3,44 +3,70 @@ import torch.nn as nn
 from modules import SelfAttention, ConvBlock, UpSampleBlock, DownSampleBlock
 
 class Enconder(nn.Module):
-    def __init__(self, in_c=3, out_c=3):
+    def __init__(self, in_c=3, out_c=3, verbose=False):
         super().__init__()
+        self.verbose = verbose
         self.conv1 = ConvBlock(in_c, 64, residual=False)
         self.down1 = DownSampleBlock(64, 128)
         self.self_attention1 = SelfAttention(128, 32)
         self.down2 = DownSampleBlock(128, 256)
         self.self_attention2 = SelfAttention(256, 16)
         self.down3 = DownSampleBlock(256, 256)
-        self.self_attention4 = SelfAttention(256, 8)
+        self.self_attention3 = SelfAttention(256, 8)
 
     def forward(self, x, t):
-        print('-- STARTING ENCODER --')
+        if self.verbose:
+            print('-- STARTING ENCODER --')
+            print(f'Shape original: {x.shape}')
         x1 = self.conv1(x)
+        if self.verbose:
+            print(f'Shape after conv1: {x1.shape}')
         x2 = self.down1(x1, t)
-        x2 = self.self_attention1(x2)
-        x3 = self.down2(x2, t)
-        x3 = self.self_attention2(x3)
-        x4 = self.down3(x3, t)
-        x4 = self.self_attention4(x4)
+        if self.verbose:
+            print(f'Shape after down1: {x2.shape}')
+        x2 = self.self_attention1(x2)        
+        if self.verbose:
+            print(f'Shape after self_attention1: {x2.shape}')
+        x3 = self.down2(x2, t)        
+        if self.verbose:
+            print(f'Shape after down2: {x3.shape}')
+        x3 = self.self_attention2(x3)        
+        if self.verbose:
+            print(f'Shape after self_attention2: {x3.shape}')
+        x4 = self.down3(x3, t)        
+        if self.verbose:
+            print(f'Shape after down3: {x4.shape}')
+        x4 = self.self_attention3(x4)        
+        if self.verbose:
+            print(f'Shape after self_attention3: {x4.shape}')
         return x1, x2, x3, x4
 
 class BottleNeck(nn.Module):
-    def __init__(self, in_c=256, out_c=256, mid_c=512):
+    def __init__(self, in_c=256, out_c=256, mid_c=512, verbose=False):
         super().__init__()
+        self.verbose = verbose
         self.bot1 = ConvBlock(in_c, mid_c, residual=False)
         self.bot2 = ConvBlock(mid_c, mid_c, residual=False)
         self.bot3 = ConvBlock(mid_c, out_c, residual=False)
 
     def forward(self, x):
-        print('-- STARTING BOTTLENECK --')
-        x = self.bot1(x)
-        x = self.bot2(x)
-        x = self.bot3(x)
+        if self.verbose:
+            print('-- STARTING BOTTLENECK --')
+        x = self.bot1(x)        
+        if self.verbose:
+            print(f'Shape after conv1: {x.shape}')
+        x = self.bot2(x)        
+        if self.verbose:
+            print(f'Shape after conv2: {x.shape}')
+        x = self.bot3(x)        
+        if self.verbose:
+            print(f'Shape after conv3: {x.shape}')
         return x
 
 class Decoder(nn.Module):
-    def __init__(self, out_c=3):
+    def __init__(self, out_c=3, verbose=False):
         super().__init__()
+        self.verbose = verbose
         self.up1 = UpSampleBlock(512, 128)
         self.self_att1 = SelfAttention(128, 16)
         self.up2 = UpSampleBlock(256, 64)
@@ -50,26 +76,41 @@ class Decoder(nn.Module):
         self.conv1 = nn.Conv2d(64, 3, kernel_size=1)
 
     def forward(self, x1, x2, x3, x4, t):
-        print('-- STARTING DECODER --')
-        x = self.up1(x4, x3, t)
-        x = self.self_att1(x)
-        x = self.up2(x, x2, t)
-        x = self.self_att2(x)
-        x = self.up3(x, x1, t)
-        x = self.self_att3(x)
-        x = self.conv1(x)
+        if self.verbose:
+            print('-- STARTING DECODER --')
+        x = self.up1(x4, x3, t)        
+        if self.verbose:
+            print(f'Shape after up1: {x.shape}')
+        x = self.self_att1(x)        
+        if self.verbose:
+            print(f'Shape after sel: {x.shape}')
+        x = self.up2(x, x2, t)        
+        if self.verbose:
+            print(f'Shape after up2: {x.shape}')
+        x = self.self_att2(x)        
+        if self.verbose:
+            print(f'Shape after self_att2: {x.shape}')
+        x = self.up3(x, x1, t)        
+        if self.verbose:
+            print(f'Shape after up3: {x.shape}')
+        x = self.self_att3(x)        
+        if self.verbose:
+            print(f'Shape after self_att3: {x.shape}')
+        x = self.conv1(x)        
+        if self.verbose:
+            print(f'Shape after conv1: {x.shape}')
         return x
 
 
 class UNet(nn.Module):
-    def __init__(self, time_dim=256, num_classes=None, device='cuda'):
+    def __init__(self, time_dim=256, num_classes=None, device='cuda', verbose=False):
         super().__init__()
-
+        self.verbose = verbose
         self.device = device
         self.time_dim = time_dim
-        self.encoder = Enconder()
-        self.bottle_neck = BottleNeck()
-        self.decoder = Decoder()
+        self.encoder = Enconder(verbose=self.verbose)
+        self.bottle_neck = BottleNeck(verbose=self.verbose)
+        self.decoder = Decoder(verbose=self.verbose)
         if num_classes is not None:
             self.label_emb = nn.Embedding(num_classes, time_dim)
 
@@ -82,8 +123,9 @@ class UNet(nn.Module):
         pos_enc = torch.cat([pos_enc_a, pos_enc_b], dim=-1)
         return pos_enc
 
-    def forward(self, x, t, y):
-        print('-> Starting UNET <-')
+    def forward(self, x, t, y=None):
+        if self.verbose:
+            print('-> Starting UNET <-')
         t = t.unsqueeze(-1).type(torch.float)
         t = self.pos_encoding(t, self.time_dim)
         if y is not None:
@@ -92,14 +134,15 @@ class UNet(nn.Module):
         x1, x2, x3, x4 = self.encoder(x, t)
         x4 = self.bottle_neck(x4)
         output = self.decoder(x1, x2, x3, x4, t)
-        print('-> Finished UNET <-')
+        if self.verbose:
+            print('-> Finished UNET <-')
         return output
 
 
 
 
 if __name__ == '__main__':
-    net = UNet(num_classes=10, device='cpu')
+    net = UNet(num_classes=10, device='cpu', verbose=True)
     print(sum([p.numel() for p in net.parameters()]))
     x = torch.randn(3, 3, 64, 64)
     t = x.new_tensor([500]*x.shape[0]).long()
